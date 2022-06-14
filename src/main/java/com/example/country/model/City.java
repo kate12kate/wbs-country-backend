98985e3eeb11f6a68188d2a7d40adbf7b3225d9d
kate12kate <44732265+kate12kate@users.noreply.github.com>
2022-06-14 14:16:27 +0200

    Implement backend

---
+++ src/main/java/com/example/country/model/City.java
@@ -0,0 +1,16 @@
+package com.example.country.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class City {
+    private String name;
+    private String description;
+    private String thumbnail;
+
+
+}
